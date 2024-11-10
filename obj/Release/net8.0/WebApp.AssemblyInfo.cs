5f12988ae436381b8834a76b38c78733c232a614
Bui-Cong-Anh <buiconganh20022010@gmail.com>
2024-11-10 20:36:40 +0700

    update blog, contact, search box

--- obj/Release/net8.0/WebApp.AssemblyInfo.cs
+++ obj/Release/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e503e2340de91b0e5c745dc0b989b8df08316b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5c90539ca9fbe3ff21788e6fe11fccaa5dd07f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
